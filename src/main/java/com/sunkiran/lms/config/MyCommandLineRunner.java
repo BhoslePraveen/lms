d55a741ac7cf11d1d4799f1e24177541cddc5f30
Praveen Bhosle <praveen.bhosle@sarvagram.com>
2025-07-01 08:16:57 +0530

    Added actuators

--- src/main/java/com/sunkiran/lms/config/MyCommandLineRunner.java
+++ src/main/java/com/sunkiran/lms/config/MyCommandLineRunner.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 
-@Configuration
+//@Configuration
 public class MyCommandLineRunner implements org.springframework.boot.CommandLineRunner{
 
     @Autowired
