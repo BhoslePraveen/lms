18383fbacdcb025ed455cc55271a8788cd791e80
Praveen Bhosle <praveen.bhosle@sarvagram.com>
2025-07-03 08:50:18 +0530

    Added Security details

--- src/main/java/com/sunkiran/lms/controller/UserController.java
+++ src/main/java/com/sunkiran/lms/controller/UserController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/user")
-public class UserController {
-
-    @PostMapping("/register")
-    public ResponseEntity<Void> postUser(@Valid @RequestBody UserDto userDto) {
-        return ResponseEntity.ok().build();
-    }
-}
+//@RestController
+//@RequestMapping("/user")
+//public class UserController {
+//
+//    @PostMapping("/register")
+//    public ResponseEntity<Void> postUser(@Valid @RequestBody UserDto userDto) {
+//        return ResponseEntity.ok().build();
+//    }
+//}
