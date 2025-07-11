fe18ffeca726a41214430449c39da51f1a918589
Praveen Bhosle <praveen.bhosle@sarvagram.com>
2025-07-11 08:28:09 +0530

    Added Eureka server

--- src/main/java/com/sunkiran/lms/SunkiranLmsApplication.java
+++ src/main/java/com/sunkiran/lms/SunkiranLmsApplication.java
@@ -2,8 +2,10 @@ package com.sunkiran.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SunkiranLmsApplication {
 
 	public static void main(String[] args) {
